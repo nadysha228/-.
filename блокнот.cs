822d74227dec345ffb3d8e6ec6cc2ccde5532c97
Надежда <147606132+nadysha228@users.noreply.github.com>
2024-12-23 10:50:01 +0300

    Add files via upload

---
+++ блокнот.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Diagnostics;
+
+class Program
+{
+    static void Main()
+    {
+        Process[] notepads = Process.GetProcessesByName("notepad");
+
+        foreach (Process notepad in notepads)
+        {
+            notepad.Kill();
+            Console.WriteLine("Notepad closed successfully");
+        }
+    }
+}
