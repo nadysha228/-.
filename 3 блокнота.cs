b0b14b3f9ce8758a4742f305d50429e69fa19d18
Надежда <147606132+nadysha228@users.noreply.github.com>
2024-12-23 11:01:46 +0300

    Add files via upload

---
+++ 3 блокнота.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Diagnostics;
+
+using System;
+using System.Diagnostics;
+
+class Program
+{
+    static void Main()
+    {
+        Process.Start("notepad.exe");
+        Process.Start("notepad.exe");
+        Process.Start("notepad.exe");
+    }
+}
