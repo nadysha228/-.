631ea7a6276d72342a55955601978d5f7ec84a45
Надежда <147606132+nadysha228@users.noreply.github.com>
2024-12-23 11:04:21 +0300

    Add files via upload

---
+++ процессы.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System;
+using System.Diagnostics;
+
+class Program
+{
+    static void Main()
+    {
+        Process[] processes = Process.GetProcesses();
+
+        Console.WriteLine("ID\tName");
+
+        foreach (Process process in processes)
+        {
+            Console.WriteLine($"{process.Id}\t{process.ProcessName}");
+        }
+    }
