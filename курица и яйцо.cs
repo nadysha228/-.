14c3428f067d5f8c35eea5d7c4792a0fffa0ffb1
Надежда <147606132+nadysha228@users.noreply.github.com>
2024-12-23 11:17:33 +0300

    Add files via upload

---
+++ курица и яйцо.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Threading;
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Создаем переменные 
+        Thread chicken = new Thread(Chicken);
+        Thread egg = new Thread(Egg);
+
+        // Запускаем потоки
+        chicken.Start();
+        egg.Start();
+
+        // Ждем завершения потоков
+        chicken.Join();
+        egg.Join();
+
+        // Проверяем, какой поток завершился последним
+        if (chicken.IsAlive)
+        {
+            Console.WriteLine("Яйцо");
+        }
+        else
+        {
+            Console.WriteLine("Курица");
+        }
+    }
+    static void Chicken()
+    {
+        // Простой вывод слова "Курица"
+        Console.WriteLine("Курица");
+    }
+
+    static void Egg()
+    {
+        // Простой вывод слова "Яйцо"
+        Console.WriteLine("Яйцо");
+    }
+}
